ccd5bff6e32b20c24c8f0af339598e5fa767277b
Tony <saric.tony@gmail.com>
2018-07-12 08:35:39 -0700

    adds checkbox option to store type, set preset array

--- storeclosingsite/controllers/postController.js
+++ storeclosingsite/controllers/postController.js
@@ -3,8 +3,11 @@
   var vm = this;
   $window.sessionStorage.userInfo ? vm.userInfo= JSON.parse($window.sessionStorage.userInfo) : vm.userInfo = {};
 
-  vm.percentOffArray = ['5','10','15','20','25','30','35','40','45','50', '55','60','65','70','75','80','85','90','95']
+  vm.percentOffArray = ['0','5','10','15','20','25','30','35','40','45','50', '55','60','65','70','75','80','85','90','95'];
+  $scope.percenthigh = vm.percentOffArray[0];
+  $scope.percentlow = vm.percentOffArray[0];
 
+  vm.storeTypes = ['clothing', 'furniture', 'tech', 'other'];
 
 
 });
